4e9ce026e3a187b9b692e6a4ef1fffcae7f9ccec
misterpah <misterpah@gmail.com>
2018-04-15 11:56:06 +0800

    add scene
    Signed-off-by: misterpah <misterpah@gmail.com>

---
+++ khaEC/Sources/khaEC/Scene.hx
@@ -0,0 +1,16 @@
+package khaEC;
+import kha.graphics2.Graphics;
+class Scene{
+	public var name:String;
+	public function new(){
+		this.name = "Scene";
+	}
+	public function update(){
+	}
+	public function destroy(){
+
+	}
+	public function render(g:Graphics){
+
+	}
+}
